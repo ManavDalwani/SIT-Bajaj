a1e851c549f543764926802cb9776bce44ef382c
Manav Dalwani <manavdalwani@gmail.com>
2026-06-10 20:56:54 +0530

    Fix CORS for Vercel frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,12 @@ const graphRoutes = require("./routes/graph");
 
 const app = express();
 
-app.use(cors());
+app.use(
+    cors({
+        origin: "*"
+    })
+);
+
 app.use(express.json());
 
 app.use("/api/graph", graphRoutes);
